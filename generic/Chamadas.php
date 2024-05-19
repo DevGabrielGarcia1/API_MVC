ec067419e64bc2315aee54c329a4c95c58dbb112
Gabriel Garcia <garcia.gabrielrodrigues1@gmail.com>
2024-05-19 07:57:53 -0300

    Add rotes

--- generic/Chamadas.php
+++ generic/Chamadas.php
@@ -13,7 +13,14 @@ class Chamadas
             "cadastrar/cliente" => new Acao("service\UsuariosService", "cadastrarCliente",[Acao::POST]),
             "cadastrar/imovel" => new Acao("service\UsuariosService", "cadastrarImovel",[Acao::POST]),
             "cadastrar/contrato" => new Acao("service\UsuariosService", "cadastrarContrato",[Acao::POST]),
-           
+            "editar/usuario" => new Acao("service\UsuariosService", "",[Acao::POST]),
+            "editar/cliente" => new Acao("service\UsuariosService", "",[Acao::POST]),
+            "editar/imovel" => new Acao("service\UsuariosService", "",[Acao::POST]),
+            "contrato/cancelar" => new Acao("service\UsuariosService", "",[Acao::POST]),
+            "remover/usuario" => new Acao("service\UsuariosService", "",[Acao::DELETE]),
+            "remover/cliente" => new Acao("service\UsuariosService", "",[Acao::DELETE]),
+            "remover/imovel" => new Acao("service\UsuariosService", "",[Acao::DELETE]),
+            "imoveis/listar" => new Acao("service\UsuariosService", "",[Acao::GET, Acao::POST], false),
         ];
     }
 
