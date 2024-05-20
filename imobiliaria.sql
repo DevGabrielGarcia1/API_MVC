ca482312bc1b5e3ae56a1383f2807cc3b6a3512b
Gabriel Garcia <garcia.gabrielrodrigues1@gmail.com>
2024-05-20 09:20:52 -0300

    Update database

--- imobiliaria.sql
+++ imobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/05/2024 às 12:45
+-- Tempo de geração: 20/05/2024 às 14:20
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -43,7 +43,9 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id`, `nome`, `CPF`, `data_nascimento`, `telefone`, `email`) VALUES
-(1, 'Teste', '00000000000', '0000-00-00', '349000000000', 'teste@gmail.com');
+(1, 'Teste', '00000000000', '0000-00-00', '349000000000', 'teste@gmail.com'),
+(8, 'Teste', '00011100011', '2024-05-20', '34991999999', 'aaaaaaaaa'),
+(9, 'Teste', '0000000000', '0024-05-20', '34567890', '3dfghj');
 
 -- --------------------------------------------------------
 
@@ -56,7 +58,7 @@ CREATE TABLE `contratos` (
   `id_imovel` int(3) NOT NULL,
   `id_cliente` int(3) NOT NULL,
   `data_inicio` date NOT NULL,
-  `data_fim` date NOT NULL,
+  `data_fim` date DEFAULT NULL,
   `forma_pagamento` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -65,7 +67,9 @@ CREATE TABLE `contratos` (
 --
 
 INSERT INTO `contratos` (`id`, `id_imovel`, `id_cliente`, `data_inicio`, `data_fim`, `forma_pagamento`) VALUES
-(1, 1, 1, '2024-05-19', '2025-05-19', 'Débito em conta');
+(1, 1, 1, '2024-05-19', '2025-05-19', 'Débito em conta'),
+(2, 3, 8, '2024-05-20', '2025-05-20', 'Pix'),
+(5, 4, 9, '2024-05-20', '2026-05-20', '20/05/2026');
 
 -- --------------------------------------------------------
 
@@ -85,15 +89,35 @@ CREATE TABLE `imoveis` (
   `quartos` int(2) DEFAULT NULL,
   `banheiros` int(2) DEFAULT NULL,
   `vagas_garagem` int(2) DEFAULT NULL,
-  `descricao` text DEFAULT NULL
+  `descricao` text DEFAULT NULL,
+  `active` int(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `imoveis`
 --
 
-INSERT INTO `imoveis` (`id`, `tipo_imovel`, `endereco`, `cidade`, `estado`, `CEP`, `valor_aluguel`, `area`, `quartos`, `banheiros`, `vagas_garagem`, `descricao`) VALUES
-(1, 'Casa', 'rua A', 'Uberaba', 'MG', '000000', 1200.00, 20.00, 2, 2, 2, 'Teste');
+INSERT INTO `imoveis` (`id`, `tipo_imovel`, `endereco`, `cidade`, `estado`, `CEP`, `valor_aluguel`, `area`, `quartos`, `banheiros`, `vagas_garagem`, `descricao`, `active`) VALUES
+(1, 'Casa', 'rua A', 'Uberaba', 'MG', '000000', 1200.00, 20.00, 2, 2, 2, 'Teste', 1),
+(3, 'Apartamento', 'Rua ABC', 'Ribeirão Preto', 'SP', '00000000', 1950.85, 50.00, 3, 2, 2, 'lsudhfdsfksd', 1),
+(4, 'Apartamento', 'Rua ABC', 'Ribeirão Preto', 'SP', '00000000', 1950.00, 50.00, 3, 2, 2, 'lsudhfdsfksd', 1),
+(5, 'Apartamento', 'Rua ABC', 'Ribeirão Preto', 'SP', '00000000', 1950.95, 50.00, 3, 2, 2, 'lsudhfdsfksd', 1),
+(6, 'Apartamento', 'Rua ABC', 'Ribeirão Preto', 'SP', '00000000', 195.50, 50.00, 3, 2, 2, 'lsudhfdsfksd', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `proprietario`
+--
+
+CREATE TABLE `proprietario` (
+  `id` int(3) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `CPF` varchar(11) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -105,16 +129,17 @@ CREATE TABLE `usuarios` (
   `id` int(2) NOT NULL,
   `username` varchar(15) NOT NULL,
   `senha` varchar(64) DEFAULT NULL,
-  `nomeCompleto` varchar(25) DEFAULT NULL
+  `permissao` int(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `username`, `senha`, `nomeCompleto`) VALUES
-(1, 'admin', 'admin', ''),
-(3, 'user', 'user', '');
+INSERT INTO `usuarios` (`id`, `username`, `senha`, `permissao`) VALUES
+(1, 'admin', 'admin', 0),
+(43, 'user', 'user', 1),
+(44, 'user2', 'user2', 1);
 
 --
 -- Índices para tabelas despejadas
@@ -141,6 +166,12 @@ ALTER TABLE `imoveis`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `proprietario`
+--
+ALTER TABLE `proprietario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -155,25 +186,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `contratos`
 --
 ALTER TABLE `contratos`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `imoveis`
 --
 ALTER TABLE `imoveis`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de tabela `proprietario`
+--
+ALTER TABLE `proprietario`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- Restrições para tabelas despejadas
